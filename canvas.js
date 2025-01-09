f41308f5ae04d43e782edfa174bac29c8145d5ee
myatt <rcoins84@yahoo.com>
2025-01-09 17:54:44 -0500

    draw rectangle

--- canvas.js
+++ canvas.js
@@ -4,6 +4,14 @@ class Canvas {
         this.element.width = 800;
         this.element.height = 600;
     }
+
+    clear() {
+        this.context.clearRect(0, 0, this.element.width, this.element.height);
+    }
+
+    get context() {
+        return this.element.getContext('2d');
+    }
 }
 
 class Menu {
@@ -28,6 +36,11 @@ class Menu {
 
 class MouseMenu extends Menu {
 
+    constructor(element, canvas) {
+        super(element);
+        canvas.element.addEventListener('mousemove', (e) => this.onMouseMove(e));
+    }
+
     show() {
         let menu = '<table>';
 
@@ -47,17 +60,67 @@ class MouseMenu extends Menu {
     }
 }
 
+class Rectangle {
+
+    constructor(canvas) {
+        canvas.element.addEventListener('mousedown', (e) => this.onMouseDown(e));
+        canvas.element.addEventListener('mouseup', (e) => this.onMouseUp(e));
+        canvas.element.addEventListener('mousemove', (e) => this.onMouseMove(e));
+    }
+
+    onMouseDown(mouseEvent) {
+        this.mouseDown = true;
+        this.initialX = mouseEvent.offsetX;
+        this.initialY = mouseEvent.offsetY;
+        this.onMouseMove(mouseEvent);
+    }
+
+    onMouseMove(mouseEvent) {
+        if (this.mouseDown) {
+            this.finalX = mouseEvent.offsetX;
+            this.finalY = mouseEvent.offsetY;
+        }
+    }
+
+    onMouseUp(mouseEvent) {
+        this.onMouseMove(mouseEvent);
+        this.mouseDown = false;
+    }
+
+    draw(context) {
+        context.fillRect(this.left, this.top, this.width, this.height);
+    }
+
+    get left() {
+        return Math.min(this.initialX, this.finalX);
+    }
+
+    get top() {
+        return Math.min(this.initialY, this.finalY);
+    }
+
+    get width() {
+        return Math.abs(this.initialX - this.finalX);
+    }
+
+    get height() {
+        return Math.abs(this.initialY - this.finalY);
+    }
+}
+
 class Screen {
     constructor() {
         this.canvas = new Canvas(document.getElementById('canvas'));
 
-        this.mouseMenu = new MouseMenu(document.getElementById('mouse'));
+        this.mouseMenu = new MouseMenu(document.getElementById('mouse'), this.canvas);
 
-        this.canvas.element.addEventListener('mousemove', (e) => this.mouseMenu.onMouseMove(e));
+        this.rectangle = new Rectangle(this.canvas);
     }
 
     tick() {
+        this.canvas.clear();
 
+        this.rectangle.draw(this.canvas.context);
     }
 }
 
