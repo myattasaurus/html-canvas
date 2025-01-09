1e9b77702362bdb87b68d5b78a299e524b709174
myatt <rcoins84@yahoo.com>
2025-01-09 13:45:49 -0500

    some styling

--- canvas.js
+++ canvas.js
@@ -1,30 +1,30 @@
 
 let canvas;
 
-window.addEventListener('mousemove', (e) => {
-    if (e.srcElement === canvas) {
-        let proto = Object.getPrototypeOf(e);
-        let descriptors = Object.getOwnPropertyDescriptors(proto);
-        let properties = '<table>';
-        for (const key of Object.keys(descriptors)) {
-            if (descriptors[key].get && !key.endsWith('Element')) {
-                properties += '<tr><td>' + key + '</td><td>' + e[key] + '</td></tr>';
-            }
-        }
-        properties += '</table>';
-
-        let show = document.getElementById('show');
-        show.innerHTML = properties;
-    }
-});
-
 function onLoad() {
     canvas = document.getElementById('canvas');
 
     canvas.height = 600;
     canvas.width = 800;
 
+    canvas.addEventListener('mousemove', onMouseMove);
+
     requestAnimationFrame(tick);
+}
+
+function onMouseMove(mouseMoveEvent) {
+    let proto = Object.getPrototypeOf(mouseMoveEvent);
+    let descriptors = Object.getOwnPropertyDescriptors(proto);
+    let properties = '<table>';
+    for (const key of Object.keys(descriptors)) {
+        if (descriptors[key].get && !key.endsWith('Element')) {
+            properties += '<tr><td>' + key + '</td><td>' + mouseMoveEvent[key] + '</td></tr>';
+        }
+    }
+    properties += '</table>';
+
+    let show = document.getElementById('show');
+    show.innerHTML = properties;
 }
 
 function tick(timestamp) {
