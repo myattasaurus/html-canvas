4afe81bce26e53ae54af03ce92a70c377b1d9da0
myatt <rcoins84@yahoo.com>
2025-01-09 15:55:15 -0500

    menu buttons

--- canvas.js
+++ canvas.js
@@ -1,6 +1,18 @@
 
 let canvas;
 
+let screen = {
+    mouse: {
+        menuVisible: false,
+        x: null,
+        y: null,
+        isDown: false
+    },
+    rectangle: {
+        menuVisible: false
+    }
+}
+
 function onLoad() {
     canvas = document.getElementById('canvas');
 
@@ -8,26 +20,53 @@ function onLoad() {
     canvas.width = 800;
 
     canvas.addEventListener('mousemove', onMouseMove);
+    window.addEventListener('mousedown', onMouseDown);
+    window.addEventListener('mouseup', onMouseUp);
 
     requestAnimationFrame(tick);
 }
 
-function onMouseMove(mouseMoveEvent) {
-    let proto = Object.getPrototypeOf(mouseMoveEvent);
-    let descriptors = Object.getOwnPropertyDescriptors(proto);
-    let properties = '<table>';
-    for (const key of Object.keys(descriptors)) {
-        if (descriptors[key].get && !key.endsWith('Element')) {
-            properties += '<tr><td>' + key + '</td><td>' + mouseMoveEvent[key] + '</td></tr>';
-        }
-    }
-    properties += '</table>';
+function onClickMouse() {
+    screen.mouse.menuVisible = !screen.mouse.menuVisible;
+}
 
-    let show = document.getElementById('show');
-    show.innerHTML = properties;
+function onMouseMove(mouseEvent) {
+    screen.mouse.x = mouseEvent.offsetX;
+    screen.mouse.y = mouseEvent.offsetY;
+}
+
+function onMouseDown(mouseEvent) {
+    screen.mouse.isDown = true;
+}
+
+function onMouseUp(mouseEvent) {
+    screen.mouse.isDown = false;
+}
+
+function showMouseMenu() {
+    let menu = '<table>';
+
+    menu += row('x', screen.mouse.x);
+    menu += row('y', screen.mouse.y);
+    menu += row('isDown', screen.mouse.isDown);
+
+    menu += '</table>';
+    document.getElementById('mouse').innerHTML = menu;
+}
+
+function hideMouseMenu() {
+    document.getElementById('mouse').innerHTML = '';
+}
+
+function row(key, value) {
+    return '<tr><td>' + key + '</td><td>' + value + '</td></tr>';
 }
 
 function tick(timestamp) {
-
+    if (screen.mouse.menuVisible) {
+        showMouseMenu();
+    } else {
+        hideMouseMenu();
+    }
     requestAnimationFrame(tick);
 }
